38460634a311a52bc12057a89b0f2401032b3674
Aditya Jadon <51057603+adityajadonsingh@users.noreply.github.com>
2025-09-15 15:34:23 +0530

    No Index

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,19 +9,23 @@ import Footer from "@/components/Footer";
 
 const lato = Lato({
   subsets: ["latin"],
-  weight: ["400","700","900"],
+  weight: ["400", "700", "900"],
   variable: "--font-lato",
 });
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
-  weight: ["400","500","700", "900"],
+  weight: ["400", "500", "700", "900"],
   variable: "--font-playfair",
 });
 
 export const metadata: Metadata = {
   title: "Stonecera",
   description: "Stonecera",
+  robots: {
+    index: false,
+    follow: false,
+  },
 };
 
 export default async function RootLayout({
@@ -33,12 +37,15 @@ export default async function RootLayout({
   const footer: FooterType = await getFooter();
   return (
     <html lang="en">
+      <head>
+        <meta name="robots" content="noindex, nofollow" />
+      </head>
       <body
         className={`${lato.variable} ${playfair.variable} relative`}
       >
         <Header allCategories={allCategories} phone={footer.phone_number1} email={footer.email1} />
         <main>{children}</main>
-        <Footer allCategories={allCategories} phone={footer.phone_number1} email={footer.email1} address={footer.address} instagram_link={footer.instagram_link} facebook_link={footer.facebook_link} linkedin_link={footer.linkedin_link} twitter_link={footer.twitter_link} pinterest_link={footer.pinterest_link}/>
+        <Footer allCategories={allCategories} phone={footer.phone_number1} email={footer.email1} address={footer.address} instagram_link={footer.instagram_link} facebook_link={footer.facebook_link} linkedin_link={footer.linkedin_link} twitter_link={footer.twitter_link} pinterest_link={footer.pinterest_link} />
       </body>
     </html>
   );
